5bc59fbd0d84e39611b2d72234a26c0fc68aa515
Ali Haider <haidet428@gmail.com>
2025-04-20 23:26:41 +0500

    now

--- FanNation/backend/src/main/java/com/example/fannation/repository/AuthenticationLogRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.fannation.repository;
-
-import com.example.fannation.entity.AuthenticationLog;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthenticationLogRepository extends JpaRepository<AuthenticationLog, Integer> {
-}
