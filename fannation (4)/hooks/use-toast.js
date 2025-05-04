7920930a9695a426b1d0f88ae32b968c4e079c57
Ali Haider <haidet428@gmail.com>
2025-05-04 19:05:53 +0500

    UI code

--- fannation (4)/hooks/use-toast.js
+++
@@ -1,7 +0,0 @@
-"use client"
-
-import { useToast as useToastOriginal } from "@/components/ui/use-toast"
-
-export const useToast = useToastOriginal
-
-export { toast } from "@/components/ui/use-toast"
--- fannation (4)/src/main/java/com/fannation/model/ERole.java
+++
@@ -1,7 +0,0 @@
-package com.fannation.model;
-
-public enum ERole {
-    ROLE_USER,
-    ROLE_MODERATOR,
-    ROLE_ADMIN
-}
